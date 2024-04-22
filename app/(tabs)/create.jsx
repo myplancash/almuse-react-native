5c4876a51bc1bba94de86aabe791fed629164c9b
sergio esteban torres <myplancash@gmail.com>
2024-04-22 09:49:26 -0500

    File & folder Structure

---
+++ app/(tabs)/create.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+//to create and add videos 
+const Create = () => {
+  return (
+    <View>
+      <Text>Create</Text>
+    </View>
+  )
+}
+
+export default Create
+
+const styles = StyleSheet.create({})
